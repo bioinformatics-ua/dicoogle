eb145e409d3c350390e284b0aed67ef35133f3ac
Eduardo Pinho <eduardopinho@ua.pt>
2015-11-17 16:07:41 +0000

    move task status to a separate component; improve code and round percentage of task progress

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/indexer/IndexStatusView.js
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/indexer/IndexStatusView.js
@@ -1,7 +1,8 @@
-var React = require('react');
+import React from 'react';
 
 import {IndexStatusActions} from "../../actions/indexStatusAction";
 import {IndexStatusStore} from "../../stores/indexStatusStore";
+import TaskStatus from "./TaskStatus.jsx";
 
 var refreshIntervalId;
 var IndexStatusView = React.createClass({
@@ -54,60 +55,9 @@ var IndexStatusView = React.createClass({
         if (this.state.data.results.length === 0) {
           items = (<div>No tasks</div>);
         } else {
-          items = this.state.data.results.map((item, index) => {
-            let complete = item.complete;
-            let unknownPercentage = item.taskProgress < 0;
-            let percentage = (complete || unknownPercentage) ? '100%'
-                : (item.taskProgress * 100 + '%');
-
-            let barstate;
-            if(item.nErrors > 0 && item.nIndexed > 0){
-              barstate = "indexprogress progress-bar progress-bar-striped progress-bar-warning";
-            }
-            else if((item.nErrors > 0) && (item.nIndexed == 0))
-            {
-              barstate = "indexprogress progress-bar progress-bar-striped progress-bar-danger";
-            }
-            else if (unknownPercentage && !complete) {
-              barstate = "indexprogress progress-bar progress-bar-striped progress-bar-info";
-            } else {
-              barstate = "indexprogress progress-bar progress-bar-striped progress-bar-success";
-            }
-            if (!complete) {
-              barstate += " active";
-            }
-            
-            return (
-              <div key={item.taskUid} className="well well-sm">
-                <div className="row">
-                    <div className="col-sm-10">
-                      <div className="progress indexstatusprogress">
-                          <div style={{width : percentage}} className={barstate} role="progressbar"  aria-valuemin="0" aria-valuemax="100">
-                                {!unknownPercentage && percentage}
-                          </div>
-                      </div>
-                    </div>
-                    <div className="col-sm-2">
-                      <button className="btn btn-danger" onClick={this.onCloseStopClicked.bind(this, complete, item.taskUid)}> {complete?"Close":"Stop"} </button>
-                    </div>
-                </div>
-
-                <div>
-                    <p><b>Uid: </b> {item.taskUid}</p>
-                    <p><b>Name: </b> {item.taskName}</p>
-                    <p style={{visibility : item.complete ? '' : 'hidden'}}>
-                        {(typeof item.elapsedTime === 'number') && (
-                          <p><b>Elapsed Time: </b> {item.elapsedTime} ms</p>)}
-                        {(typeof item.nIndexed === 'number') && (
-                          <p><b>Indexed: </b> {item.nIndexed} </p>)}
-                        {(typeof item.nErrors === 'number') && (
-                          <p><b>Errors: </b> {item.nErrors} </p>)}
-                    </p>
-                </div>
-
-              </div>
-            );
-          });
+          items = this.state.data.results.map(item => (
+            <TaskStatus index={item.taskUid} item={item} onCloseStopClicked={this.onCloseStopClicked.bind(this, item.taskUid, item.complete)} />
+          ));
         }
         return (
           <div className="">
@@ -142,7 +92,7 @@ var IndexStatusView = React.createClass({
       onStartClicked : function(){
         IndexStatusActions.start(document.getElementById("path").value);
       },
-      onCloseStopClicked : function(type, uid){
+      onCloseStopClicked : function(uid, type){
         if(type){
           IndexStatusActions.close(uid);
         }
---
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/indexer/TaskStatus.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+const {PropTypes} = React;
+
+class TaskStatus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  
+  static get propTypes() {
+    return {
+      item : PropTypes.shape({
+          taskUid: PropTypes.string.isRequired,
+          complete: PropTypes.bool,
+          taskProgress: PropTypes.number,
+          elapsedTime: PropTypes.number,
+          nIndexed: PropTypes.number,
+          nErrors: PropTypes.number
+        }).isRequired,
+      onCloseStopClicked: PropTypes.func.isRequired
+    };
+  }
+	
+  render() {
+    const {item, onCloseStopClicked} = this.props;
+    const {complete} = item;
+    const unknownPercentage = (typeof item.taskProgress !== 'number' || item.taskProgress < 0);
+    const percentage = (complete || unknownPercentage) ? '100%'
+      : (Math.round(item.taskProgress * 100) + '%');
+
+    let barstate = "indexprogress progress-bar progress-bar-striped";
+    if (item.nErrors > 0 && item.nIndexed > 0) {
+      barstate += " progress-bar-warning";
+    } else if((item.nErrors > 0) && (item.nIndexed == 0)) {
+      barstate += " progress-bar-danger";
+    } else if (unknownPercentage && !complete) {
+      barstate += " progress-bar-info active";
+    } else {
+      barstate += " progress-bar-success";
+      if (!complete) {
+        barstate += " active";
+      }
+    }
+
+  return (
+     <div key={item.taskUid} className="well well-sm">
+       <div className="row">
+      <div className="col-sm-10">
+        <div className="progress indexstatusprogress">
+          <div style={{width : percentage}} className={barstate} role="progressbar"  aria-valuemin="0" aria-valuemax="100">
+            {!unknownPercentage && percentage}
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-2">
+        <button className="btn btn-danger" onClick={onCloseStopClicked}>
+          {complete ? "Close" : "Stop"}
+        </button>
+      </div>
+    </div>
+    <div>
+      <p><b>Uid: </b> {item.taskUid}</p>
+      <p><b>Name: </b> {item.taskName}</p>
+      <p style={{visibility : item.complete ? '' : 'hidden'}}>
+          {(typeof item.elapsedTime === 'number') && (
+            <p><b>Elapsed Time: </b> {item.elapsedTime} ms</p>)}
+          {(typeof item.nIndexed === 'number') && (
+            <p><b>Indexed: </b> {item.nIndexed} </p>)}
+          {(typeof item.nErrors === 'number') && (
+            <p><b>Errors: </b> {item.nErrors} </p>)}
+        </p>
+      </div>
+    </div>);
+	}
+}
+
+export default TaskStatus;
