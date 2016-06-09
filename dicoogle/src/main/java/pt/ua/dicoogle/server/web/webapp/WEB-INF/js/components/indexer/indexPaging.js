0fc8bd0d40d71733a97598f58b3dcd0d39775141
Luís A. Bastião Silva <luis.kop@gmail.com>
2016-06-09 20:53:11 +0100

    Added class for management indexing tasks pages.

---
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/indexer/indexPaging.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {IndexStatusActions} from "../../actions/indexStatusAction";
+import {IndexStatusStore} from "../../stores/indexStatusStore";
+import TaskStatus from "./TaskStatus.jsx";
+
+
+/**
+ * Page Indexing Tasks
+ * - Page where all tasks will be paged
+ * - It should be possible clean also the accomplished tasks.  
+ */
+
+
+
+export default class IndexingPaging extends React.Component {
+
+  static get propTypes() {
+    return {
+
+    };
+  }
+}
