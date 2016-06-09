e2e570ada8034ff45c7d0583b0ad2372dcf6ec60
Luis A. Bastiao Silva <bastiao@bmd-software.com>
2016-06-09 19:33:44 +0100

    Changed the colors.

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/search/searchResult.jsx
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/search/searchResult.jsx
@@ -199,13 +199,12 @@ const Step = React.createClass({
     if (this.props.searchOutcome.data!=null )
       numResults = this.props.searchOutcome.data.numResults;
 
-
     return (
       <div className="row">
         <div className="wizardbar">
           <div className={this.getStep(this.state.current, 0)}>
             <div onClick={this.onStepClicked.bind(this, 0)}>
-             <span className="label label-primary label-as-badge label-border">{numResults}</span> Patient</div>
+             <span className="label label-info label-as-badge label-border">{numResults}</span> Patient</div>
           </div>
           <div className={this.getStep(this.state.current, 1)}>
             <div onClick={this.onStepClicked.bind(this, 1)}>Study</div>
