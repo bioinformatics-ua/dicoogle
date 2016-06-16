3abd0cb3e7a26b2188a0e7c788c8c3cfd4691e20
Luis A. Bastiao Silva <bastiao@bmd-software.com>
2016-06-16 14:58:21 +0100

    removed the trigger delay.

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/stores/searchStore.js
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/stores/searchStore.js
@@ -29,8 +29,8 @@ const SearchStore = Reflux.createStore({
           data["advancedOptions"] = self._contents.advancedOptions;
           self._contents = data;
 
-          //DEBUG WAIT
-          setTimeout(self.triggerWithDelay, 50)
+          //Trigger search
+          self.triggerWithDelay()
         },
         function(xhr){
           //FAILURE
