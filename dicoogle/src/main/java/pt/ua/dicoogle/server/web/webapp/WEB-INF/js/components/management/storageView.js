04ee9ef0599332a4cb6aab68778340de903e7090
Eduardo Pinho <eduardopinho@ua.pt>
2016-11-20 21:57:22 +0000

    Improve storage servers UI
    - modify storage store to support description+public fields
    - validade fields of new server form
    - refine rendering of new server form
    - refactor related components
        - remove use of Component.isMounted in related components

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/management/storageView.js
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/webapp/WEB-INF/js/components/management/storageView.js
@@ -2,68 +2,142 @@ import React from 'react';
 
 import {StorageActions} from '../../actions/storageActions';
 import {StorageStore} from '../../stores/storageStore';
-import {Button, Modal} from 'react-bootstrap';
+import {Button, Modal, FormGroup, FormControl, ControlLabel, Checkbox} from 'react-bootstrap';
 
 const AddStorageModal = React.createClass({
+    getInitialState() {
+      return {
+        aetitle: '',
+        ip: '',
+        port: '',
+        public: false
+      };
+    },
+
   render: function() {
-    return(<Modal {...this.props} bsStyle='primary' title='Add storage server' animation>
-
-      <div className='modal-body'>
+    return(<Modal {...this.props} bsStyle='primary' animation>
+      <Modal.Header>
+        <Modal.Title>Add Storage Server</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
         <div>
-          AETitle
-          <input id="storage_aetitle" className="form-control" style={{width: '100%'}} type="text" placeholder="aetitle" />
-          <br></br>
-          Ip Address
-          <input id="storage_ip" className="form-control" style={{width: '100%'}} type="text" placeholder="ip address" />
-          <br></br>
-          Port
-          <input id="storage_port" className="form-control" style={{width: '100%'}} type="text" placeholder="port" />
+          <FormGroup validationState={this.validateAETitle()}>
+            <ControlLabel>AE Title</ControlLabel>
+            <FormControl style={{width: '100%'}} type="text"
+                    placeholder="AE Title" onChange={this.handleChangeAETitle}
+                    value={this.state.aetitle} />
+          </FormGroup>
+          <FormGroup validationState={this.validateIPAddress()}>
+            <ControlLabel>IP Address</ControlLabel>
+            <FormControl style={{width: '100%'}} type="text"
+                  placeholder="IP Address" onChange={this.handleChangeIPAddress} />
+          </FormGroup>
+          <FormGroup validationState={this.validatePort()}>
+            <ControlLabel>Port</ControlLabel>
+            <FormControl style={{width: '100%'}} type="text"
+                  placeholder="Port" onChange={this.handleChangePort}
+                  onKeyDown={this.handleFieldKeyDown} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Description</ControlLabel>
+            <FormControl style={{width: '100%'}} type="text"
+                  onChange={this.handleChangeDescription} onKeyDown={this.handleFieldKeyDown} />
+          </FormGroup>
+          <FormGroup>
+            <Checkbox inline value={this.state.public} onChange={this.handleChangePublic}>
+              Public
+            </Checkbox>
+          </FormGroup>
         </div>
-
-      </div>
-      <div className='modal-footer'>
-        <Button onClick={this.onAdd}>Add</Button>
-      </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button bsStyle="primary" disabled={!this.validateAll()} onClick={this.handleAdd}>Add</Button>
+        <Button onClick={this.handleAdd}>Cancel</Button>
+      </Modal.Footer>
     </Modal>);
   },
-
-  onAdd () {
-    console.log("onAdd clicked");
-    StorageActions.add(
-      document.getElementById("storage_aetitle").value,
-      document.getElementById("storage_ip").value,
-      document.getElementById("storage_port").value);
-      this.props.onHide();
+  
+  handleFieldKeyDown(e) {
+    if (e.keyCode === 13) {
+      if (this.validateAll()) {
+        this.handleAdd();
+      }
     }
+  },
+
+  handleChangeAETitle(e) {
+    this.setState({aetitle: e.target.value.toUpperCase()});
+  },
+  handleChangeIPAddress(e) {
+    this.setState({ip: e.target.value});
+  },
+  handleChangePort(e) {
+    this.setState({port: e.target.value});
+  },
+  handleChangeDescription(e) {
+    this.setState({description: e.target.value});
+  },
+  handleChangePublic(e) {
+    this.setState({public: !this.state.public});
+  },
+
+  validateAETitle(input) {
+    input = (input || this.state.aetitle).trim();
+    if (input === '') return undefined;
+    return input.length <= 16 ? 'success' : 'error';
+  },
+  validateIPAddress(input) {
+    input = (input || this.state.ip).trim();
+    return input.length > 0 ? 'success' : undefined;
+  },
+  validatePort(input) {
+    input = (input || this.state.port).trim();
+    if (input === '') return undefined;
+    const v = +input;
+    return (v > 0 && v < 65536) ? 'success' : 'error';
+  },
+  validateAll() {
+    return this.validateAETitle(this.state.aetitle) === 'success'
+        && this.validateIPAddress(this.state.ip) === 'success'
+        && this.validatePort(this.state.port) === 'success';
+  },
+
+  handleAdd () {
+    const {aetitle, ip, port, description} = this.state;
+    StorageActions.add(aetitle, ip, port, description, this.state.public);
+    this.props.onHide();
+  }
 });
 
 const StorageView = React.createClass({
-  getInitialState: function() {
-    return {
-      data: [],
-      showAdd: false,
-      status: "loading"};
+    getInitialState: function() {
+      return {
+        data: [],
+        showAdd: false,
+        status: "loading",
+        selectedIndex: null
+      };
     },
-    componentDidMount: function(){
+    componentDidMount() {
       StorageActions.get();
-      //$("#consolediv").scrollTop($("#consolediv")[0].scrollHeight);
     },
-    componentWillMount: function() {
-      StorageStore.listen(this._onChange);
+    componentWillMount() {
+      this.unsubscribe = StorageStore.listen(this._onChange);
+    },
+    componentWillUnmount() {
+      this.unsubscribe();
     },
     _onChange: function(data){
-      if (this.isMounted()) {
-        this.setState({data: data.data});
-      }
+      this.setState({data: data.data});
     },
 
     render: function() {
-      const moves = this.state.data.map((item, index) => {
-          return (
-            <option key={index} value={index}>
-              {item.AETitle + "@" + item.ipAddrs + ":" + item.port}
-            </option>);
-      });
+      const moves = this.state.data.map((item, index) => (
+            <option key={index} value={index} onClick={this.handleSelect}>
+              {item.aetitle + " @ " + item.ip + ":" + item.port}
+              {item.description && (" - " + item.description)}
+              {item.public && ' (public)'}
+            </option>));
 
       return (
 
@@ -72,17 +146,21 @@ const StorageView = React.createClass({
             Storage Servers
           </div>
           <div className="panel-body">
-            <select defaultValue={0} className="form-control" id="moves" size={6} style={{width: '100%'}}>
+            <select defaultValue={0} className="form-control" size={6} style={{width: '100%'}}>
               {moves}
             </select>
-            <div style={{textAlign: 'left'}}>
-              <button className="btn btn_dicoogle" onClick={this.onAdd}>Add new</button>
-              <button className="btn btn_dicoogle" onClick={this.onRemove}>Remove</button>
+            <div style={{textAlign: 'left', marginTop: '8px'}}>
+              <button className="btn btn_dicoogle" onClick={this.onAdd}>Add New</button>
+              <button className="btn btn_dicoogle" disabled={typeof this.state.selectedIndex !== 'number'}
+                      onClick={this.onRemove}>Remove</button>
             </div>
           </div>
           <AddStorageModal show={this.state.showAdd} onHide={this.onHideAdd} />
         </div>
       );
+    },
+    handleSelect(e) {
+      this.setState({selectedIndex: +e.target.value});
     },
     onAdd() {
       this.setState({showAdd: true});
@@ -91,7 +169,7 @@ const StorageView = React.createClass({
       this.setState({showAdd: false});
     },
     onRemove() {
-      var index = document.getElementById("moves").selectedIndex;
+      const index = this.state.selectedIndex;
       StorageActions.remove(index);
     }
 
