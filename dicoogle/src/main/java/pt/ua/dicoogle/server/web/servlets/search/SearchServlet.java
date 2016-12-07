c9d7c14432f20407ed77897a6d42cb223251bf58
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2016-12-07 18:36:13 +0000

    fix #136

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/servlets/search/SearchServlet.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/servlets/search/SearchServlet.java
@@ -163,11 +163,22 @@ public class SearchServlet extends HttpServlet {
             List<String> activeProviders = PluginController.getInstance().getQueryProvidersName(true);
             for (String p : providers) {
                 if (activeProviders.contains(p)) {
-                    knownProviders.add(p);
+                	
+                    knownProviders.add(p);}
+                else
+                {
+                	response.setStatus(400);
+                    JSONObject obj = new JSONObject();
+                    obj.put("error", p.toString() +" is not a valid query provider");
+                    response.getWriter().append(obj.toString());
+                	return;
                 }
+                    
+                
             }
         }
-
+        //known providers= empty array list message
+        
         HashMap<String, String> extraFields = new HashMap<>();
         if (actualFields == null) {
             
@@ -197,7 +208,9 @@ public class SearchServlet extends HttpServlet {
             Iterable<SearchResult> results;
             if (queryAllProviders) {
                 results = PluginController.getInstance().queryAll(queryTaskHolder, query, extraFields).get();
-            } else {
+            }
+            
+            else {
                 results = PluginController.getInstance().query(queryTaskHolder, knownProviders, query, extraFields).get();
             }
 
