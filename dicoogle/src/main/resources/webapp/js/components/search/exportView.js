7c825a904a38d5e599614af82ec1bc0ec6c58822
Leonardo Oliveira <leo.rafa96@hotmail.com>
2017-12-18 15:36:19 +0000

    Allow to load and export user's presets in the webappp

--- dicoogle/src/main/resources/webapp/js/components/search/exportView.js
+++ dicoogle/src/main/resources/webapp/js/components/search/exportView.js
@@ -1,31 +1,78 @@
 import React from 'react';
-import {Button, Modal, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import Select from 'react-select';
+import {Button, Modal, FormGroup, FormControl, ControlLabel, Form} from 'react-bootstrap';
 
 import {ExportActions} from '../../actions/exportActions';
 import {ExportStore} from '../../stores/exportStore';
+import $ from 'jquery';
 
 const ExportView = React.createClass({
 	getInitialState: function() {
 		return {
-			data: [],
+			fields: [],
+			presets: [],
 			status: "loading",
-			fieldText: "",
+
+      selectedFields: [],
+      selectedPresetName: "",
+      exportPresetName: "default",
+
+      actionPerformed: false,
+
 			current: 0
 		};
 	},
-	componentWillMount: function() {
+
+  componentWillMount: function() {
 		// Subscribe to the store.
 		console.log("subscribe listener");
-		this.unsubscribe = ExportStore.listen(this._onChange);
+    this.unsubscribe = ExportStore.listen(this._onChange);
 	},
+
+  componentDidMount: function() {
+    ExportActions.getFieldList();
+    ExportActions.getPresets();
+  },
+
   componentWillUnmount() {
     this.unsubscribe();
   },
+
 	_onChange: function(data){
-		this.setState({data: data.data, status: "done"});
+    if (!data.success) {
+      this.setState({
+        status: "failed"
+      });
+      return;
+    }
+
+    let fields = data.data.fields;
+    let presets = data.data.presets;
+
+    let status = this.state.status;
+    if (fields && presets) status = "done";
+
+    this.setState({
+      fields: fields,
+      presets: presets,
+      status: status
+    });
+
+
+    if (this.state.actionPerformed) {
+      this.setState({
+        actionPerformed: false
+      });
+
+      let toastMessage = this.state.error ? this.state.error : "Saved.";
+      $('.toast').stop().text(toastMessage).fadeIn(400).delay(3000).fadeOut(400); // fade out after 3 seconds
+    }
 	},
 
 	render: function() {
+    let fieldList = this.state.fields.map(field => ({value: field, label: field}));
+    let presetList = this.state.presets.map(preset => ({value: preset.name, label: preset.name}));
+
     return (
 			<Modal {...this.props} bsStyle='primary' animation>
 				<Modal.Header>
@@ -33,33 +80,85 @@ const ExportView = React.createClass({
 				</Modal.Header>
 				<Modal.Body>
 					<FormGroup>
-						<ControlLabel>Export Field List</ControlLabel>
-						<FormControl
-							componentClass="textarea"
-							placeholder="Paste export fields here (one per line) ..."
-							rows="10"
-							bsClass="exportlist"
-							onChange={this.handleTextChanged}
-							value={this.state.fieldText}
-						/>
+            <ControlLabel>Preset (optional):</ControlLabel>
+            <Select simpleValue
+                    id="selected-preset"
+                    value={this.state.selectedPresetName}
+                    options={presetList}
+                    placeholder="Choose a preset"
+                    onChange={this.handlePresetSelect}
+            />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Fields to export:</ControlLabel>
+            <Select multi
+                    id="selected-fields"
+                    value={this.state.selectedFields}
+                    options={fieldList}
+                    placeholder="Choose fields"
+                    onChange={this.handleFieldSelect}
+            />
 					</FormGroup>
 				</Modal.Body>
 				<Modal.Footer id="hacked-modal-footer-do-not-remove">
-					<Button bsStyle="primary" onClick={this.handleExportClicked}>Export</Button>
-				</Modal.Footer>
+          <Form inline>
+            <FormControl
+              type="text"
+              value={this.state.exportPresetName}
+              placeholder="Name the preset"
+              onChange={this.handlePresetNameChange}
+              maxLength="100"
+            />
+            <Button bsStyle="default" onClick={this.handleSavePresetClicked} disabled={!this.canExport()}>Save Preset</Button>
+            <Button bsStyle="primary" onClick={this.handleExportClicked} disabled={!this.canSave()}>Export</Button>
+          </Form>
+          <div className="toast">Saved</div>
+        </Modal.Footer>
 			</Modal>)
 	},
 
-	handleTextChanged: function(e) {
-		this.setState({fieldText: e.target.value});
-	},
+  handleFieldSelect: function(fields) {
+    this.setState({
+      selectedFields: fields.map((e) => e.value)
+    });
+  },
+
+  handlePresetSelect: function(name) {
+    let fields = this.state.presets.filter(preset => preset.name === name)[0].fields;
+
+    this.setState({
+      exportPresetName: name,
+      selectedPresetName: name,
+      selectedFields: fields
+    });
+  },
+
+  handlePresetNameChange: function(e) {
+    this.setState({
+      exportPresetName: e.target.value
+    });
+  },
+
+  handleSavePresetClicked: function() {
+    ExportActions.savePresets(this.state.exportPresetName, this.state.selectedFields);
+
+    this.setState({
+      actionPerformed: true
+    });
+  },
 
   handleExportClicked: function() {
-    const fields = this.state.fieldText.split("\n");
-    console.log(fields);
-
-    const query = this.props.query;
+    let fields = this.state.selectedFields;
+    let query = this.props.query;
     ExportActions.exportCSV(query, fields);
+  },
+
+  canSave: function () {
+    return this.state.selectedFields.length !== 0;
+  },
+
+  canExport: function () {
+    return this.state.exportPresetName && this.canSave();
   }
 });
 
--- dicoogle/src/main/resources/webapp/js/stores/exportStore.js
+++ dicoogle/src/main/resources/webapp/js/stores/exportStore.js
@@ -5,6 +5,7 @@ import {Endpoints} from '../constants/endpoints';
 import {getDICOMFieldList} from '../handlers/requestHandler';
 
 import dicoogleClient from 'dicoogle-client';
+import {UserStore} from "./userStore";
 
 const ExportStore = Reflux.createStore({
     listenables: ExportActions,
@@ -14,7 +15,7 @@ const ExportStore = Reflux.createStore({
       this.dicoogle = dicoogleClient();
     },
 
-    onGetFieldList: function(data){
+    onGetFieldList: function() {
       var self = this;
 
       getDICOMFieldList((error, data) => {
@@ -26,8 +27,7 @@ const ExportStore = Reflux.createStore({
           return;
         }
 
-        // console.log("success", data);
-        self._contents = data;
+        self._contents.fields = JSON.parse(data).sort();
         self.trigger({
           data: self._contents,
           success: true
@@ -58,7 +58,54 @@ const ExportStore = Reflux.createStore({
         link.click();
         hacked_footer.removeChild(link);
       });
+    },
+
+    onGetPresets: function () {
+      let self = this;
+      let username = UserStore._username;
+
+      this.dicoogle.request('GET', ['presets', username])
+        .end((error, data) => {
+          if (error) {
+            self.trigger({
+              success: false,
+              status: error.status
+            });
+            return;
+          }
+
+          self._contents.presets = JSON.parse(data.text);
+          self.trigger({
+            data: self._contents,
+            success: true
+          });
+        });
+    },
+
+    onSavePresets: function (name, fields) {
+      let self = this;
+
+      let queryParams = "";
+      fields.forEach((field) => (queryParams += "field=" + field + "&"));
+      if (queryParams.length !== 0) queryParams.slice(0, -1);
+
+      let username = UserStore._username;
+
+      this.dicoogle
+        .request('POST', ['presets', username, name])
+        .query(queryParams)
+        .then((res) => {
+          if (res.status !== 200) {
+            self.trigger({
+              success: false,
+              status: res.status
+            });
+            return;
+          }
+
+          // refresh list of presets
+          self.onGetPresets();
+        })
     }
 });
-
 export {ExportStore};
