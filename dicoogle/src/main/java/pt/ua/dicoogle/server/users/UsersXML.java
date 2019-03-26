e1e4d638cc8ece823a090d852beac29e50cdbef0
Rui Lebre <ruilebre@ua.pt>
2019-03-26 13:28:34 +0000

    removed <p> tag removed unused import

--- dicoogle/src/main/java/pt/ua/dicoogle/server/users/UsersXML.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/users/UsersXML.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2014  Universidade de Aveiro, DETI/IEETA, Bioinformatics Group - http://bioinformatics.ua.pt/
- * <p>
+ *
  * This file is part of Dicoogle/dicoogle.
- * <p>
+ *
  * Dicoogle/dicoogle is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * Dicoogle/dicoogle is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,7 +25,6 @@ import java.io.PrintWriter;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.LoggerFactory;
