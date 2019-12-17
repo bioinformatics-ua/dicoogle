736a86c6b49a1ecf4af83805f1c136b3bd47dc90
Rui Lebre <ruilebre@ua.pt>
2019-12-17 16:44:24 +0000

    updated Dicoogle js client request to the last version of the API

--- dicoogle/src/main/resources/webapp/js/stores/pluginStore.js
+++ dicoogle/src/main/resources/webapp/js/stores/pluginStore.js
@@ -2,40 +2,40 @@ import Reflux from "reflux";
 import { Endpoints } from "../constants/endpoints";
 import * as PluginActions from "../actions/pluginActions";
 
-//import {request} from '../handlers/requestHandler';
-
 import dicoogleClient from "dicoogle-client";
 const Dicoogle = dicoogleClient(Endpoints.base);
 
 const PluginStore = Reflux.createStore({
   listenables: PluginActions,
-  init: function() {
+  init: function () {
     this._contents = {};
   },
 
-  onGet: function(type) {
+  onGet: function (type) {
     const self = this;
 
-    Dicoogle.request("GET", ["plugins", type], (error, data) => {
+    Dicoogle.request("GET", ["plugins", type]).end(function (error, data) {
       if (!error) {
-        self._contents[type] = data.plugins;
+        //console.log(data.body);
+        self._contents[type] = data.body.plugins;
         self.trigger({
           data: self._contents[type],
           success: true
         });
       } else {
+        //console.log(error.body);
         self.trigger({
           success: false,
-          status: error.status
+          status: error.body.status
         });
       }
     });
   },
 
-  onSetAction: function(type, name, action) {
+  onSetAction: function (type, name, action) {
     const self = this;
 
-    Dicoogle.request("POST", ["plugins", type, name, action], (error, data) => {
+    Dicoogle.request("POST", ["plugins", type, name, action]).end(function (error, data) {
       if (!error) {
         self._contents[type].find(plugin => plugin.name === name).enabled =
           action === "enable";
