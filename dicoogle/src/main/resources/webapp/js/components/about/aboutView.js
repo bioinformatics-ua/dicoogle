d2cc713800c1a9cd67b9edd3b607e4797f3dcc72
Eduardo Pinho <eduardopinho@bmd-software.com>
2021-01-03 17:22:58 +0000

    Fix About component
    - react-bootstrap now uses Panel.Heading child element for headers

--- dicoogle/src/main/resources/webapp/js/components/about/aboutView.js
+++ dicoogle/src/main/resources/webapp/js/components/about/aboutView.js
@@ -22,115 +22,120 @@ const AboutView = createReactClass({
     this.unsubscribe();
   },
   render: function() {
-    let versionNumber = this.state.version;
-    var title = <h3>Dicoogle PACS, version: {versionNumber}</h3>;
-    var licenses = (
-        <Grid className="">
-          <Row className="show-grid">
-            <Col className="gridAbout" xs={2} md={2}>
-              <b>dcm4che2</b>
-              <br />License: GPL
-            </Col>
-            <Col className="gridAbout" xs={2} md={2}>
-              <b>react.js+reflux</b>
-              <br />License: BSD
-            </Col>
-            <Col className="gridAbout" xs={2} md={2}>
-              <b>Jetty</b>
-              <br />License: GPL
-            </Col>
-          </Row>
-        </Grid>
-    );
+    const versionNumber = this.state.version;
 
     var panelsInstance = (
-        <div className="about">
-          <Panel header={title} bsStyle="primary">
-            Dicoogle is an open-source PACS archive software that replaces the traditional database model
-            with an extensible indexing and retrieval framework and provides easy
-            expansion of functionalities through the use of plug-ins. It was
-            designed to accommodate automatic information extraction,
-            indexing, and storage of all meta-data detected in medical images, without re-engineering or reconfiguration requirements,
-            thus overcoming the limitations of traditional DICOM query
-            services. By presenting the technical assets for plugin development
-            such as a Software Development Kit (SDK),
-            developers are free to expand the archive independently and
-            non-exclusively, without changes to the core platform. This extensible architecture of Dicoogle has enabled its use
-            in research and the healthcare industry, as many use cases can
-            be fulfilled in the same deployment
-            <br />
-          </Panel>
+      <div className="about">
+        <Panel bsStyle="primary">
+          <Panel.Heading>
+            <h4>Dicoogle, version {versionNumber}</h4>
+          </Panel.Heading>
+          Dicoogle is an open source medical imaging repository with an
+          extensible indexing system and distributed mechanisms. Our solution
+          can be used as a PACS archive, or as a client for reading your PACS
+          archive file system, thus allowing you to do PACS mining. Moreover, it
+          can be easily extended with your own pluggable components. At present,
+          we have already indexed around 22 million DICOM images, and this
+          number tends to increase. There are several researchers working to
+          evaluate and improve the quality of medical records, and Dicoogle has
+          contributed to many of such case studies.
+          <br />
+        </Panel>
 
-          <Panel header="Main third party components" bsStyle="primary">
-            {licenses}
-            Note: Although these are not the only components used, these are
-            considered the main ones.
-          </Panel>
+        <Panel bsStyle="primary">
+          <Panel.Heading>
+            <h4>Main third party components</h4>
+          </Panel.Heading>
+          <Grid>
+            <Row className="show-grid">
+              <Col className="gridAbout" xs={2} md={2}>
+                <b>dcm4che2</b>
+                <br />License: GPL
+              </Col>
+              <Col className="gridAbout" xs={2} md={2}>
+                <b>React</b>
+                <br />License: MIT
+              </Col>
+              <Col className="gridAbout" xs={2} md={2}>
+                <b>Jetty</b>
+                <br />License: GPL
+              </Col>
+            </Row>
+          </Grid>
+          Note: Although these are not the only components used, these are
+          considered the main ones.
+        </Panel>
 
-          <Panel header="Disclaimer" bsStyle="primary">
-            This software is provided by the copyright holders and contributors
-            "as is" and any express or implied warranties, including, but not
-            limited to, the implied warranties of merchantability and fitness for
-            a particular purpose are disclaimed. In no event shall the copyright
-            owner or contributors be liable for any direct, indirect, incidental,
-            special, exemplary, or consequential damages (including, but not
-            limited to, procurement of substitute goods or services; loss of use,
-            data, or profits; or business interruption) however caused and on any
-            theory of liability, whether in contract, strict liability, or tort
-            (including negligence or otherwise) arising in any way out of the use
-            of this software, even if advised of the possibility of such damage.
-          </Panel>
-          <Panel header="Developers" bsStyle="primary">
-            As an open source software, Dicoogle can accept contributions from
-            developers around the world. Dicoogle OSS is led and supported by
-            Bioinformatics UA and <a target="_new" href="http://www.bmd-software.com">
-            BMD Software</a>. Please check{" "} <a target="_new" href="http://www.dicoogle.com">
+        <Panel bsStyle="primary">
+          <Panel.Heading>
+            <h4>Disclaimer</h4>
+          </Panel.Heading>
+          This software is provided by the copyright holders and contributors
+          "as is" and any express or implied warranties, including, but not
+          limited to, the implied warranties of merchantability and fitness for
+          a particular purpose are disclaimed. In no event shall the copyright
+          owner or contributors be liable for any direct, indirect, incidental,
+          special, exemplary, or consequential damages (including, but not
+          limited to, procurement of substitute goods or services; loss of use,
+          data, or profits; or business interruption) however caused and on any
+          theory of liability, whether in contract, strict liability, or tort
+          (including negligence or otherwise) arising in any way out of the use
+          of this software, even if advised of the possibility of such damage.
+        </Panel>
+        <Panel bsStyle="primary">
+          <Panel.Heading>
+            <h4>Developers</h4>
+          </Panel.Heading>
+          As an open source software, Dicoogle can accept contributions from
+          developers around the world. Dicoogle OSS is led and supported by
+          Bioinformatics UA and BMD Software. Please check{" "}
+          <a target="_new" href="http://www.dicoogle.com">
             the Dicoogle website
           </a>{" "}
-            or our{" "}
-            <a
-                target="_new"
-                href="http://www.github.com/bioinformatics-ua/dicoogle"
-            >
-              GitHub repository
-            </a>{" "}
-            for more information.
-            <div style={{ display: "inline-block", width: "100%" }}>
-              <a href="http://bioinformatics.ua.pt">
-                <img
-                    src="assets/logos/logobio.png"
-                    style={{ height: 40, margin: 5 }}
-                />
-              </a>
-              <a href="http://bmd-software.com/">
-                <img
-                    src="assets/logos/logo.png"
-                    style={{ height: 40, padding: 5, margin: 5 }}
-                />
-              </a>
-              <a href="http://www.ieeta.pt/">
-                <img
-                    src="assets/logos/logo-ieeta.png"
-                    style={{ height: 60, margin: 5 }}
-                />
-              </a>
-              <a href="http://www.ua.pt/">
-                <img
-                    src="assets/logos/logo-ua.png"
-                    style={{ height: 60, margin: 5 }}
-                />
-              </a>
-            </div>
-            <div style={{ display: "inline-block" }}>
-              <a>
-                <img
-                    src="assets/logos/logoFCT.png"
-                    style={{ height: 30, margin: 5 }}
-                />
-              </a>
-            </div>
-          </Panel>
-        </div>
+          or our{" "}
+          <a
+            target="_new"
+            href="http://www.github.com/bioinformatics-ua/dicoogle"
+          >
+            GitHub repository
+          </a>{" "}
+          for more information.
+          <div style={{ }}>
+            <a href="https://bioinformatics.ua.pt">
+              <img
+                src="assets/logos/logobio.png"
+                style={{ height: "3em", margin: 5 }}
+              />
+            </a>
+            <a href="https://bmd-software.com/">
+              <img
+                src="assets/logos/logo.png"
+                style={{ height: "3em", margin: 5 }}
+              />
+            </a>
+            <a href="https://www.ieeta.pt/">
+              <img
+                src="assets/logos/logo-ieeta.png"
+                style={{ height: "5em", margin: 5 }}
+              />
+            </a>
+            <a href="https://www.ua.pt/">
+              <img
+                src="assets/logos/logo-ua.png"
+                style={{ height: "5em", margin: 5 }}
+              />
+            </a>
+          </div>
+          <div style={{ display: "inline-block" }}>
+            <a>
+              <img
+                src="assets/logos/logoFCT.png"
+                style={{ height: "4em", margin: 5 }}
+              />
+            </a>
+          </div>
+        </Panel>
+      </div>
     );
     return panelsInstance;
   }
