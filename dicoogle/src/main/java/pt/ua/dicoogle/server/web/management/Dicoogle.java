1f538b0035a118c5f9ac9ccbcb83be3b978f721c
Luís Bastião Silva <bastiao@bmd-software.com>
2021-06-21 15:19:07 +0100

    Added the support for XRay Dose Report SOP Class UID

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/management/Dicoogle.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/management/Dicoogle.java
@@ -545,8 +545,10 @@ public class Dicoogle
 			sopClasses.put(UID.BasicVoiceAudioWaveformStorage, "BasicVoiceAudioWaveformStorage");
 			sopClasses.put(UID.HangingProtocolStorage, "HangingProtocolStorage");
 			sopClasses.put(UID.SiemensCSANonImageStorage, "SiemensCSANonImageStorage");
-                        sopClasses.put(UID.VLWholeSlideMicroscopyImageStorage, "VLWholeSlideMicroscopyImageStorage");
-                        sopClasses.put(UID.BreastTomosynthesisImageStorage, "BreastTomosynthesisImageStorage");
+			sopClasses.put(UID.VLWholeSlideMicroscopyImageStorage, "VLWholeSlideMicroscopyImageStorage");
+			sopClasses.put(UID.BreastTomosynthesisImageStorage, "BreastTomosynthesisImageStorage");
+			sopClasses.put(UID.XRayRadiationDoseSRStorage, "XRayRadiationDoseSRStorage");
+
 
 			transferSettings.put(UID.ImplicitVRLittleEndian, "ImplicitVRLittleEndian");
 			transferSettings.put(UID.ExplicitVRLittleEndian, "ExplicitVRLittleEndian");
