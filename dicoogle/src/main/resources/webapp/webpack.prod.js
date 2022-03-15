f9938f98ab353f19a6a2f0b273af9d49f40092a2
Eduardo Pinho <eduardopinho@bmd-software.com>
2022-03-15 12:19:57 +0000

    Update webapp dependencies
    - corejs 3
       - tweaked so that only the needed polyfills are added
    - webpack 5
    - remove react-font-awesome
       - it was not used in practice
    - babel minor bump
    - replace .babelrc with babel.config.json
    - replace `babel-eslint` with `@babel/eslint-parser`
    - css-loader 6
    - eslint 7
    - sass bump

--- dicoogle/src/main/resources/webapp/webpack.prod.js
+++ dicoogle/src/main/resources/webapp/webpack.prod.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 
-const merge = require("webpack-merge");
+const {merge} = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
