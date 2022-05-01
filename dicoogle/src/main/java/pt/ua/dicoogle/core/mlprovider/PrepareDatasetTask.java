758bccb68e8b08ad44aa51c1d1a4e7b400fa96dc
Rui Jesus <r.jesus@bmd-software.com>
2022-05-01 16:02:51 +0100

    Implemented new ImageWorker interface

--- dicoogle/src/main/java/pt/ua/dicoogle/core/mlprovider/PrepareDatasetTask.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/core/mlprovider/PrepareDatasetTask.java
@@ -1,26 +1,15 @@
 package pt.ua.dicoogle.core.mlprovider;
 
-import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.Tag;
-import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
-import org.dcm4che3.imageio.plugins.dcm.DicomImageReader;
-import org.dcm4che3.imageio.plugins.dcm.DicomMetaData;
 import org.dcm4che3.io.DicomInputStream;
 import pt.ua.dicoogle.plugins.PluginController;
 import pt.ua.dicoogle.sdk.datastructs.SearchResult;
-import pt.ua.dicoogle.sdk.datastructs.dim.BulkAnnotation;
+import pt.ua.dicoogle.sdk.imageworker.ImageROI;
+import pt.ua.dicoogle.sdk.imageworker.ImageWorkerInterface;
 import pt.ua.dicoogle.sdk.mlprovider.MLDataset;
 import pt.ua.dicoogle.sdk.mlprovider.MLImageDataset;
-import pt.ua.dicoogle.sdk.mlprovider.MLlabel;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Callable;
@@ -41,114 +30,37 @@ public class PrepareDatasetTask implements Callable<MLDataset> {
     @Override
     public MLDataset call() throws Exception {
 
-        MLImageDataset mlDataset = new MLImageDataset();
-        ImageReader imageReader = getImageReader();
+        HashMap<String, String> extraFields = new HashMap<String, String>();
 
+        extraFields.put("SOPInstanceUID", "SOPInstanceUID");
+        extraFields.put("SharedFunctionalGroupsSequence_PixelMeasuresSequence_PixelSpacing", "SharedFunctionalGroupsSequence_PixelMeasuresSequence_PixelSpacing");
+        extraFields.put("Rows", "Rows");
+        extraFields.put("Columns", "Columns");
+        extraFields.put("NumberOfFrames", "NumberOfFrames");
+        extraFields.put("TotalPixelMatrixColumns", "TotalPixelMatrixColumns");
+        extraFields.put("TotalPixelMatrixRows", "TotalPixelMatrixRows");
+        extraFields.put("ImageType", "ImageType");
+
+        MLImageDataset mlDataset = new MLImageDataset();
+
+        ImageWorkerInterface worker = controller.getImageWorkerInterfaceByName("roiExtractor", true);
 
         this.request.getDataset().entrySet().forEach((entry -> {
             try {
 
-                Attributes dataset;
-                DicomMetaData dicomMetaData;
-                DicomImageReadParam param;
-
                 // Query this image using the first available query provider
                 Iterable<SearchResult> results = controller
-                        .query(controller.getQueryProvidersName(true).get(0), "SOPInstanceUID:" + entry.getKey()).get();
+                        .query(controller.getQueryProvidersName(true).get(0), "SOPInstanceUID:" + entry.getKey(), extraFields).get();
 
                 for (SearchResult image : results) {
-                    DicomInputStream dis = new DicomInputStream(new File(image.getURI()));
-
-                    Attributes fmi = dis.getFileMetaInformation();
-                    dataset = dis.readDataset(-1, -1);
-                    dicomMetaData = new DicomMetaData(fmi, dataset);
-
-                    int tile_height = fmi.getInt(Tag.Rows, -1);
-                    int tile_width = fmi.getInt(Tag.Columns, -1);
-
-                    imageReader.setInput(dicomMetaData);
-                    param = (DicomImageReadParam) imageReader.getDefaultReadParam();
-
-                    List<MLlabel> labels;
-
-                    for (BulkAnnotation annotation : entry.getValue()) {
-                        labels = new ArrayList<>();
-                        List<List<Integer>> frameMatrix = getFrameMatrixFromAnnotation(fmi, annotation);
-                        BufferedImage combined = new BufferedImage(frameMatrix.size() * tile_width,
-                                frameMatrix.get(0).size() * tile_height, BufferedImage.TYPE_INT_RGB);
-                        Graphics g = combined.getGraphics();
-                        int c = -1;
-                        int d = 0;
-                        for (List<Integer> row : frameMatrix) {
-                            c++;
-                            for (Integer frame : row) {
-                                BufferedImage bb = imageReader.read(frame, param);
-                                g.drawImage(bb, c * tile_width, d++ * tile_height, null);
-                            }
-                        }
-                        g.dispose();
-                        // Save as new image
-                        File f = new File(dataset + File.separator + entry.getKey() + ".jpg");
-                        ImageIO.write(combined, "jpg", f);
-
-                        MLlabel label = new MLlabel(annotation.getLabel(), 1);
-                        labels.add(label);
-                        mlDataset.getDataset().put(f.toURI(), labels);
-                    }
-
+                    List<ImageROI> rois = (List<ImageROI>) worker.extractROIs(image, entry.getValue());
                 }
-            } catch (InterruptedException | ExecutionException | IOException e) {
+
+            } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace(); // Ignore
             }
         }));
 
         return mlDataset;
     }
-
-    private static ImageReader getImageReader() {
-        Iterator<ImageReader> iter = ImageIO.getImageReadersByFormatName("DICOM");
-        while (iter.hasNext()) {
-            ImageReader reader = iter.next();
-            if (reader instanceof DicomImageReader)
-                return reader;
-        }
-        return null;
-    }
-
-    private List<List<Integer>> getFrameMatrixFromAnnotation(Attributes attrs, BulkAnnotation annotation) {
-        int image_width = attrs.getInt(Tag.TotalPixelMatrixColumns, -1);
-
-        int tile_height = attrs.getInt(Tag.Rows, -1);
-        int tile_width = attrs.getInt(Tag.Columns, -1);
-
-        int nx_tiles = (int) Math.ceil(image_width / tile_width);
-
-        List<List<Integer>> matrix = new ArrayList<>();
-
-        switch (annotation.getAnnotationType()) {
-            case RECTANGLE:
-                // Get dimmensions of the annotation
-                double width = annotation.getPoints().get(0).distance(annotation.getPoints().get(1));
-                double height = annotation.getPoints().get(0).distance(annotation.getPoints().get(3));
-
-                // Estimate the number of frames that the annotation covers in height and width
-                int xframes = (int) Math.ceil(width / tile_width);
-                int yframes = (int) Math.ceil(height / tile_height);
-
-                // Calculate the starting position of the annotation in frame coordinates
-                int x_c = (int) Math.floor(annotation.getPoints().get(0).getX() / tile_width);
-                int y_c = (int) Math.floor(annotation.getPoints().get(0).getX() / tile_width);
-
-                for (int i = y_c; i < (yframes + y_c); i++) {
-                    matrix.add(new ArrayList<>());
-                    for (int j = x_c; j < (xframes + x_c); j++) {
-                        matrix.get(i - y_c).add(i * nx_tiles + j);
-                    }
-                }
-                break;
-        }
-
-        return matrix;
-    }
-
 }
--- sdk/src/main/java/pt/ua/dicoogle/sdk/imageworker/ImageROI.java
+++ sdk/src/main/java/pt/ua/dicoogle/sdk/imageworker/ImageROI.java
@@ -10,4 +10,33 @@ public class ImageROI {
 
     private URI roi;
 
+    public ImageROI(int width, int height, URI roi) {
+        this.width = width;
+        this.height = height;
+        this.roi = roi;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public URI getRoi() {
+        return roi;
+    }
+
+    public void setRoi(URI roi) {
+        this.roi = roi;
+    }
 }
