2362a8154a9fc40545601205200d9d9e4dc4ad71
Rui Jesus <r.jesus@bmd-software.com>
2022-11-18 10:35:07 +0000

    Baseline ROI extractor servlet. Fully removed ImageWorkerInterface.

--- dicoogle/src/main/java/pt/ua/dicoogle/plugins/PluginController.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/plugins/PluginController.java
@@ -30,7 +30,6 @@ import pt.ua.dicoogle.sdk.*;
 import pt.ua.dicoogle.sdk.datastructs.Report;
 import pt.ua.dicoogle.sdk.datastructs.SearchResult;
 import pt.ua.dicoogle.sdk.datastructs.dim.DimLevel;
-import pt.ua.dicoogle.sdk.imageworker.ImageWorkerInterface;
 import pt.ua.dicoogle.sdk.settings.ConfigurationHolder;
 import pt.ua.dicoogle.sdk.task.JointQueryTask;
 import pt.ua.dicoogle.sdk.task.Task;
@@ -345,19 +344,6 @@ public class PluginController {
         return this.getServletPlugins(true);
     }
 
-    public Collection<ImageWorkerInterface> getImageWorkerPlugins(boolean onlyEnabled) {
-        List<ImageWorkerInterface> plugins = new ArrayList<>();
-        for (PluginSet pSet : pluginSets) {
-            for (ImageWorkerInterface i : pSet.getImageWorkerPlugins()) {
-                if (!i.isEnabled() && onlyEnabled) {
-                    continue;
-                }
-                plugins.add(i);
-            }
-        }
-        return plugins;
-    }
-
     public Collection<String> getPluginSetNames() {
         Collection<String> l = new ArrayList<>();
         for (PluginSet s : this.pluginSets) {
@@ -523,18 +509,6 @@ public class PluginController {
             }
         }
         logger.debug("No indexer matching name {} for onlyEnabled = {}", name, onlyEnabled);
-        return null;
-    }
-
-    public ImageWorkerInterface getImageWorkerInterfaceByName(String name, boolean onlyEnabled) {
-        Collection<ImageWorkerInterface> plugins = getImageWorkerPlugins(onlyEnabled);
-        for (ImageWorkerInterface p : plugins) {
-            if (p.getName().equalsIgnoreCase(name)) {
-                // logger.info("Retrived Query Provider: "+name);
-                return p;
-            }
-        }
-        logger.debug("No image worker matching name {} for onlyEnabled = {}", name, onlyEnabled);
         return null;
     }
 
--- sdk/src/main/java/pt/ua/dicoogle/sdk/datastructs/wsi/WSISopDescriptor.java
+++ sdk/src/main/java/pt/ua/dicoogle/sdk/datastructs/wsi/WSISopDescriptor.java
@@ -1,2 +1,74 @@
-package pt.ua.dicoogle.sdk.datastructs.wsi;public class WSISopDescriptor {
+package pt.ua.dicoogle.sdk.datastructs.wsi;
+
+import org.dcm4che2.data.Tag;
+import org.dcm4che3.data.Attributes;
+/**
+ * Utility class to describe a specific resolution level of a WSI pyramid
+ */
+public class WSISopDescriptor {
+
+    private int tileWidth;
+    private int tileHeight;
+    private int totalPixelMatrixColumns;
+    private int totalPixelMatrixRows;
+
+    public WSISopDescriptor(int tileWidth, int tileHeight, int totalPixelMatrixColumns, int totalPixelMatrixRows) {
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
+        this.totalPixelMatrixColumns = totalPixelMatrixColumns;
+        this.totalPixelMatrixRows = totalPixelMatrixRows;
+    }
+
+    public WSISopDescriptor() {
+    }
+
+    public int getTileWidth() {
+        return tileWidth;
+    }
+
+    public void setTileWidth(int tileWidth) {
+        this.tileWidth = tileWidth;
+    }
+
+    public int getTileHeight() {
+        return tileHeight;
+    }
+
+    public void setTileHeight(int tileHeight) {
+        this.tileHeight = tileHeight;
+    }
+
+    public int getTotalPixelMatrixColumns() {
+        return totalPixelMatrixColumns;
+    }
+
+    public void setTotalPixelMatrixColumns(int totalPixelMatrixColumns) {
+        this.totalPixelMatrixColumns = totalPixelMatrixColumns;
+    }
+
+    public int getTotalPixelMatrixRows() {
+        return totalPixelMatrixRows;
+    }
+
+    public void setTotalPixelMatrixRows(int totalPixelMatrixRows) {
+        this.totalPixelMatrixRows = totalPixelMatrixRows;
+    }
+
+    /**
+     * Given a search result, extract if possible the information that describes the resolution level contained within.
+     * @param attrs
+     */
+    public void extractData(Attributes attrs){
+        String strRows = attrs.getString(Tag.Rows);
+        String strColumns = attrs.getString(Tag.Columns);
+        String strTotalPixelMatrixColumns = attrs.getString(Tag.TotalPixelMatrixColumns);
+        String strTotalPixelMatrixRows = attrs.getString(Tag.TotalPixelMatrixRows);
+
+        this.totalPixelMatrixColumns = Integer.parseInt(strTotalPixelMatrixColumns);
+        this.totalPixelMatrixRows = Integer.parseInt(strTotalPixelMatrixRows);
+        this.tileHeight = Integer.parseInt(strRows.split("\\.")[0]);
+        this.tileWidth = Integer.parseInt(strColumns.split("\\.")[0]);
+    }
+
 }
+
