2e0ea4e81ab277a570894da9087105510b9b098b
Rui Jesus <r.jesus@bmd-software.com>
2022-11-23 04:50:05 +0000

    Fixed compilation issues.

--- dicoogle/src/test/java/pt/ua/dicoogle/core/plugins/PlatformInterfaceMock.java
+++ dicoogle/src/test/java/pt/ua/dicoogle/core/plugins/PlatformInterfaceMock.java
@@ -26,7 +26,6 @@ import pt.ua.dicoogle.sdk.core.DicooglePlatformInterface;
 import pt.ua.dicoogle.sdk.datastructs.Report;
 import pt.ua.dicoogle.sdk.datastructs.SearchResult;
 import pt.ua.dicoogle.sdk.datastructs.dim.DimLevel;
-import pt.ua.dicoogle.sdk.imageworker.ImageWorkerInterface;
 import pt.ua.dicoogle.sdk.settings.server.ServerSettingsReader;
 import pt.ua.dicoogle.sdk.task.JointQueryTask;
 import pt.ua.dicoogle.sdk.task.Task;
@@ -123,16 +122,6 @@ public class PlatformInterfaceMock implements DicooglePlatformInterface {
 
     @Override
     public List<Report> indexBlocking(URI path) {
-        return null;
-    }
-
-    @Override
-    public ImageWorkerInterface getImageWorkerByName(String name, boolean onlyEnabled) {
-        return null;
-    }
-
-    @Override
-    public Collection<ImageWorkerInterface> getImageWorkers(boolean onlyEnabled) {
         return null;
     }
 
