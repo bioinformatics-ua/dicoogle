3bc7d30f0fe9e7fb1e7353a012268cf52635aaf9
Rui-Jesus <r.jesus@bmd-software.com>
2023-06-07 17:24:26 +0100

    Improvements to datastore endpoint. New Train endpoint.

--- dicoogle/src/main/java/pt/ua/dicoogle/server/web/servlets/mlprovider/TrainServlet.java
+++ dicoogle/src/main/java/pt/ua/dicoogle/server/web/servlets/mlprovider/TrainServlet.java
@@ -1,12 +1,12 @@
 package pt.ua.dicoogle.server.web.servlets.mlprovider;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pt.ua.dicoogle.core.mlprovider.DatastoreRequest;
+import pt.ua.dicoogle.core.mlprovider.TrainRequest;
 import pt.ua.dicoogle.plugins.PluginController;
+import pt.ua.dicoogle.sdk.mlprovider.MLProviderInterface;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,9 +14,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class DatastoreServlet extends HttpServlet {
+public class TrainServlet extends HttpServlet {
 
-    private static final Logger log = LoggerFactory.getLogger(DatastoreServlet.class);
+    private static final Logger log = LoggerFactory.getLogger(TrainServlet.class);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,18 +28,20 @@ public class DatastoreServlet extends HttpServlet {
         }
 
         ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        DatastoreRequest datasetRequest;
+        TrainRequest trainRequest;
         try {
-            datasetRequest = mapper.readValue(dataString, DatastoreRequest.class);
-            /*
-            if(PluginController.getInstance().getMachineLearningProviderByName(datasetRequest.getProviderName(), true) == null){
-                resp.sendError(404, "The requested provider does not exist");
-            }*/
-            PluginController.getInstance().datastore(datasetRequest);
+            trainRequest = mapper.readValue(dataString, TrainRequest.class);
+            MLProviderInterface mlplugin = PluginController.getInstance().getMachineLearningProviderByName(trainRequest.getProvider(), true);
+            if(mlplugin == null){
+                log.error("A provider with the provided name does not exist");
+                resp.sendError(404, "Malformed request");
+            } else {
+                mlplugin.trainModel(trainRequest.getModelID());
+            }
         } catch (Exception e) {
             log.error("Error parsing json string", e);
             resp.sendError(404, "Malformed request");
         }
     }
+
 }
