0baf3fb12f81128b6a10a97924f5344eb773c4c9
Rui-Jesus <r.jesus@bmd-software.com>
2023-07-25 12:15:07 +0100

    Added license headers

--- sdk/src/main/java/pt/ua/dicoogle/sdk/mlprovider/MLModelTrainInfo.java
+++ sdk/src/main/java/pt/ua/dicoogle/sdk/mlprovider/MLModelTrainInfo.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2014  Universidade de Aveiro, DETI/IEETA, Bioinformatics Group - http://bioinformatics.ua.pt/
+ *
+ * This file is part of Dicoogle/dicoogle-sdk.
+ *
+ * Dicoogle/dicoogle-sdk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dicoogle/dicoogle-sdk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package pt.ua.dicoogle.sdk.mlprovider;
 
 import java.util.Date;
