f9f4b8f842039561773637faed86ca1ea1e28d6f
Eduardo Pinho <eduardopinho@bmd-software.com>
2023-10-04 14:57:48 +0100

    [webapp] Remove advanced search UI
    It was misleading because it hasn't worked for a long time.

--- dicoogle/src/main/resources/webapp/js/components/search/searchView.jsx
+++ dicoogle/src/main/resources/webapp/js/components/search/searchView.jsx
@@ -6,7 +6,6 @@ import Autosuggest from "react-autosuggest";
 import { ActionCreators } from "../../actions/searchActions";
 import { ProvidersStore } from "../../stores/providersStore";
 import { ProvidersActions } from "../../actions/providersActions";
-import { AdvancedSearch } from "../search/advancedSearch";
 import { SearchResult } from "../search/searchResult";
 import { DimFields } from "../../constants/dimFields";
 import { getUrlVars } from "../../utils/url";
@@ -24,7 +23,6 @@ const Search = createReactClass({
     let fields = DimFields.map(field => ({ value: field, label: field }));
     return {
       label: "login",
-      searchState: "simple",
       providers: [],
       selectedProviders: [],
       queryText: "",
@@ -83,35 +81,22 @@ const Search = createReactClass({
     }));
 
     let selectionButtons = (
-      <div>
-        <div className="row">
-          <div className="col-md-2 col-sm-3">
-            <button
-              type="button"
-              className="btn btn_dicoogle btn-block"
-              onClick={this.renderFilter}
-              data-trigger="advance-search"
-              id="btn-advance"
-            >
-              {this.state.searchState === "simple" ? "Advanced" : "Basic"}
-            </button>
-          </div>
-          <div className="col-md-4 col-sm-9">
-            <Select
-              multi
-              id="providersList"
-              name="form-field-name"
-              value={this.state.selectedProviders}
-              options={providersList}
-              placeholder="All Providers"
-              onChange={this.handleProviderSelect}
-            />
-          </div>
+      <div className="row">
+        <div className="col-md-4 col-sm-9">
+          <Select
+            multi
+            id="providersList"
+            name="form-field-name"
+            value={this.state.selectedProviders}
+            options={providersList}
+            placeholder="All Providers"
+            onChange={this.handleProviderSelect}
+          />
         </div>
       </div>
     );
 
-    let simpleSearchInstance = (
+    let searchInstance = (
       <div className="row space_up" id="main-search">
         <form onSubmit={this.onSearchClicked}>
           <div className="col-xs-8 col-sm-10">
@@ -152,25 +137,14 @@ const Search = createReactClass({
         />
       );
     }
-    if (this.state.searchState === "simple") {
-      return (
-        <div>
-          {selectionButtons}
-          {simpleSearchInstance}
-          {resultComponent}
-          <div className="result-error">{this.state.error}</div>
-        </div>
-      );
-    } else if (this.state.searchState === "advanced") {
-      return (
-        <div>
-          {selectionButtons}
-          <AdvancedSearch />
-          {resultComponent}
-          <div className="result-error">{this.state.error}</div>
-        </div>
-      );
-    }
+    return (
+      <div>
+        {selectionButtons}
+        {searchInstance}
+        {resultComponent}
+        <div className="result-error">{this.state.error}</div>
+      </div>
+    );
   },
 
   onSuggestionsFetchRequested: function(value) {
@@ -244,15 +218,6 @@ const Search = createReactClass({
       requestedQuery: error ? null : this.state.requestedQuery,
       error
     });
-  },
-  renderFilter: function() {
-    var switchState;
-    if (this.state.searchState === "simple") {
-      switchState = "advanced";
-    } else {
-      switchState = "simple";
-    }
-    this.setState({ searchState: switchState });
   },
   onSearchByUrl: function() {
     let params = {
